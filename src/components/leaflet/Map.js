b4a2147f9af0e3356596ed2b0ca2dcf7c40609fa
Brendan Ward <bcward@consbio.org>
2017-07-21 16:03:42 -0700

    Added vector tiles and event handling

--- src/components/leaflet/Map.js
+++ src/components/leaflet/Map.js
@@ -1,28 +1,24 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {scaleLinear} from 'd3-scale';
 import L from 'leaflet';
+import 'leaflet.vectorgrid';
 import 'leaflet-basemaps';
-//import '../node_modules/leaflet-modal';
-// import '../../../node_modules/leaflet.locatecontrol';
-// postCSS import of Leaflet's CSS
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-basemaps/L.Control.Basemaps.css';
 import './Map.css';
-// require('leaflet-basemaps')
 
 
 // Map configurationParameters
 let config = {
     mapParams: {
         center: [33.358, -78.593],
-        zoom: 6,
-        minZoom: 6,
-        maxZoom: 14,
+        zoom: 10, //5
+        minZoom: 3,
+        maxZoom: 15,
         zoomControl: false,
         scrollwheel: false,
-        // legends: true,
-        // infoControl: false,
         attributionControl: false,
-        // layers: [grayscale],
         zIndex: 1
     },
     basemaps: [
@@ -35,109 +31,54 @@ let config = {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
             label: 'ESRI Imagery'
         })
-    ]
-
-
-
-
-
+    ],
+    blueprintLayer: L.tileLayer('http://52.43.202.160/services/blueprint_2_1/tiles/{z}/{x}/{y}.png', {
+        maxNativeZoom: 13,
+        bounds: [[29.257276664000074, -85.89853697199993], [37.45876403900007, -71.28723321899992]],
+        zIndex: 2
+    }),
+    unitLayer: L.vectorGrid.protobuf('http://10.5.1.165:8000/services/salcc_id/tiles/{z}/{x}/{y}.pbf', {
+        minZoom: 9,
+        maxZoom: 15,
+        zIndex: 3,
+        rendererFactory: L.svg.tile,
+        interactive: true,
+        vectorTileLayerStyles: {
+            inland_marine_id: {
+                fill: true,
+                fillOpacity: 0,
+                weight: 1,
+                color: 'orange'
+            }
+        },
+        getFeatureId: (f) => {return f.properties.ID}
+    }),
+    highlightStyle: {
+        color: 'orange',
+        fillColor: 'orange',
+        fillOpacity: 0.4,
+        fill: true,
+        weight: 2
+    }
 
 };
 
-
-
-
-// config.tileLayer = {
-//     uri: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-//     params: {
-//         minZoom: 5,
-//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-//         id: '',
-//         accessToken: ''
-//     }
-// };
-
-// array to store unique names of Brooklyn subway lines,
-// this eventually gets passed down to the Filter component
-
-//class Map extends Component {
-export default class Map extends Component {
+class Map extends Component {
     constructor(props) {
         super(props);
-        // this.state = {
-        //   map: null,
-        //   // tileLayer: null,
-        // };
+        this.state = {
+            selectedID: null
+        }
+
         this._mapNode = null;
-        // this.updateMap = this.updateMap.bind(this);
-        // this.onEachFeature = this.onEachFeature.bind(this);
-        //this.pointToLayer = this.pointToLayer.bind(this);//not needed
-        //this.filterFeatures = this.filterFeatures.bind(this);//not needed
-
+        this.map = null;
     }
-
 
     componentDidMount() {
-        // if (!this.state.map) this.init(this._mapNode);
-
-        this.init();
+        let map = this.map = L.map(this._mapNode, config.mapParams);
 
 
-
-
-        // var blueprintLayer = new L.tileLayer(blueprintTilesURL, {
-        //     maxZoom: 13,
-        //     maxNativeZoom: 11,
-        //     errorTileUrl: 'static/blank.png',
-        //     bounds: [[29.257276664000074, -85.89853697199993], [37.45876403900007, -71.28723321899992]],
-        //     zIndex: 2
-        // }).addTo(map);
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        // code to run when the component receives new props or state
-        // check to see if geojson is stored, map is created, and geojson overlay needs to be added
-
-    }
-
-    componentWillUnmount() {
-        // code to run just before unmounting the component
-        // this destroys the Leaflet map object & related event listeners
-        // this.state.map.remove();
-    }
-
-    // getData() {
-    //     // could also be an AJAX request that results in setting state with the geojson data
-    //     // for simplicity sake we are just importing the geojson data using webpack's json loader
-    //
-    // }
-    //
-    // updateMap(e) {
-    //
-    // }
-    //
-    //
-    // zoomToFeature(target) {///////////////////////////////////////////////////////////////////////////////////unsure
-    //     // pad fitBounds() so features aren't hidden under the Filter UI element
-    //     var fitBoundsParams = {
-    //         paddingTopLeft: [200, 10],
-    //         paddingBottomRight: [10, 10]
-    //     };
-    //     // set the map's center & zoom so that it fits the geographic extent of the layer
-    //     this.state.map.fitBounds(target.getBounds(), fitBoundsParams);
-    // }
-    //
-    // onEachFeature(feature, layer) {//not needed////////////
-    // }
-
-    init() {
-        // if (this.state.map) return;
-        // this function creates the Leaflet map object and is called after the Map component mounts
-        let map = L.map(this._mapNode, config.mapParams);
-
-
-        var basemapsControl = L.control.basemaps({
+        let basemapsControl = L.control.basemaps({
             basemaps: config.basemaps,
             tileX: 4,
             tileY: 6,
@@ -146,13 +87,45 @@ export default class Map extends Component {
         });
         map.addControl(basemapsControl);
 
+        map.addLayer(config.blueprintLayer);
+
+        let opacityScale = scaleLinear().domain([3,13]).range([1, 0.35]);
+        config.blueprintLayer.setOpacity(opacityScale(map.getZoom()));  // do this on initial load too
+        map.on('zoomend', function(){
+            config.blueprintLayer.setOpacity(opacityScale(map.getZoom()));
+        });
+
+
+
+        config.unitLayer.on('click', (f) => {
+            this._handleSelect(f.layer.properties.ID);
+        });
+
+        map.addLayer(config.unitLayer);
+
+
         // L.control.locate({position: "bottomright"}).addTo(map);
 
-        // a TileLayer is used as the "basemap"
-        // const tileLayer = L.tileLayer(config.tileLayer.uri, config.tileLayer.params).addTo(map);
+    }
 
-        // set our state to include the tile layer
-        // this.setState({ map, tileLayer });
+
+    _handleSelect(id) {
+        console.log(this.state.selectedID, id)
+        if (this.state.selectedID !== null) {
+            if (id === this.state.selectedID) {
+                // clear highlight
+                this.setState({selectedID: null});
+                config.unitLayer.resetFeatureStyle(id);
+                this.props.deselectUnit();
+                return;
+            }
+
+            config.unitLayer.resetFeatureStyle(this.state.selectedID);
+        }
+
+        this.setState({selectedID: id});
+        config.unitLayer.setFeatureStyle(id, config.highlightStyle)
+        this.props.selectUnit(id); // move this to callback above
     }
 
 
@@ -161,7 +134,21 @@ export default class Map extends Component {
             <div id="MapContainer">
                 <div ref={(node) => this._mapNode = node} id="Map" />
             </div>
-
         );
     }
 }
+
+Map.propTypes = {
+    selectUnit: PropTypes.func,
+    deselectUnit: PropTypes.func
+}
+
+
+
+Map.defaultProps = {
+    selectUnit: (id) => {console.log('Selected map unit: ', id)},
+    deselectUnit: () => {console.log('Deselected map unit')}
+}
+
+
+export default Map;
