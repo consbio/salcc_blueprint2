a2bdc79e370f02b086a726beb6b4961dd55fd69e
Natasha Anisimova <anisimon@oregonstate.edu>
2017-08-04 14:43:01 -0700

    minor styling fixes

--- src/components/HomeTab.jsx
+++ src/components/HomeTab.jsx
@@ -29,29 +29,29 @@ function HomeTab() {
                 </p>
                 <h4>Priority Categories</h4>
                 <div>
-                    <div className="flex-container">
-                        <div className="flex-item"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAGxJREFUOI3t1KEOgDAMBNATSyqXGRz8BB9IP3RiCjmCPIeiYbabIjvVVLycuoDBCe8hItoDkVQDRURX7kdEcmEnCqpkkFRrGJGwYHM3rMiwhiMzwQlO8GfgieJGblwtSFKrZJsgT5qB/T568wAWSyFwDyoiLwAAAABJRU5ErkJggg==" alt=""></img></div>
-                        <div className="flex-item"> <b>  Highest priority:</b> the most important areas for natural and cultural resources based on
+                    <div className="flex-container2">
+                        <div className="flex-item2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAGxJREFUOI3t1KEOgDAMBNATSyqXGRz8BB9IP3RiCjmCPIeiYbabIjvVVLycuoDBCe8hItoDkVQDRURX7kdEcmEnCqpkkFRrGJGwYHM3rMiwhiMzwQlO8GfgieJGblwtSFKrZJsgT5qB/T568wAWSyFwDyoiLwAAAABJRU5ErkJggg==" alt=""></img></div>
+                        <div className="flex-item2"> <b>  Highest priority:</b> the most important areas for natural and cultural resources based on
                                 indicator condition. This class covers 10% of the South Atlantic geography.</div>
                     </div>
                     <br/>
-                    <div className="flex-container">
-                        <div className="flex-item"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAHJJREFUOI1jYaAyYIEx2NnZGygx6OfPnw1wA9nZ2RuyGNzq5bnFyDLs7Ne7DKvYjzP8/PmzAe5CeW4xBlN2NbJduOrrcQa4C6kJRg0cNXDUwGFm4Nmvd8k25OHXV6gG/vz5s2EV+3F4EUQOQClgkQUoBQBcnCRdcyUxFQAAAABJRU5ErkJggg==" alt=""></img></div>
-                        <div className="flex-item"><b>  High priority: </b>important areas for natural and cultural resources based on indicator
+                    <div className="flex-container2">
+                        <div className="flex-item2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAHJJREFUOI1jYaAyYIEx2NnZGygx6OfPnw1wA9nZ2RuyGNzq5bnFyDLs7Ne7DKvYjzP8/PmzAe5CeW4xBlN2NbJduOrrcQa4C6kJRg0cNXDUwGFm4Nmvd8k25OHXV6gG/vz5s2EV+3F4EUQOQClgkQUoBQBcnCRdcyUxFQAAAABJRU5ErkJggg==" alt=""></img></div>
+                        <div className="flex-item2"><b>  High priority: </b>important areas for natural and cultural resources based on indicator
                          condition. This class covers an additional 15% of the South Atlantic geography; together,
                          the highest and high priority categories cover 25%.</div>
                     </div>
                     <br/>
-                    <div className="flex-container">
-                        <div className="flex-item"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAHJJREFUOI1jYaAyYIEx2NnZGygx6OfPnw1wA9nZ2Ru6E5Lr1WVlyTLs4MWLDL2b1jP8/PmzAe5CdVlZBmdDY7Jd2LtpPQPchdQEowaOGjhq4DAz8ODFi2QbcvPxY1QDf/782dC7aT28CCIHoBSwyAKUAgA94iTl61gEtQAAAABJRU5ErkJggg==" alt=""></img></div>
-                        <div className="flex-item"><b>  Medium priority: </b>above-average areas for natural and cultural resources based on indicator
+                    <div className="flex-container2">
+                        <div className="flex-item2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAHJJREFUOI1jYaAyYIEx2NnZGygx6OfPnw1wA9nZ2Ru6E5Lr1WVlyTLs4MWLDL2b1jP8/PmzAe5CdVlZBmdDY7Jd2LtpPQPchdQEowaOGjhq4DAz8ODFi2QbcvPxY1QDf/782dC7aT28CCIHoBSwyAKUAgA94iTl61gEtQAAAABJRU5ErkJggg==" alt=""></img></div>
+                        <div className="flex-item2"><b>  Medium priority: </b>above-average areas for natural and cultural resources based on indicator
                         condition, capturing potential restoration opportunities. This class covers 20% of the South
                         Atlantic geography; together, the highest, high, and medium priority categories cover 45%.</div>
                     </div>
                     <br/>
-                    <div className="flex-container">
-                        <div className="flex-item"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAGdJREFUOI3t1CEOwDAMA8CAScHh/UUfuDzUID8oNhtatNK0aKpRFHAy8iWbc72HqvoKRNITVFXvvd9mVsIiQgAISc+GZiattXJDAJINd+aABzzgz8CIKCNjjBkk6QBygiqZBvb7WM0DvcUmwcYVKdQAAAAASUVORK5CYII=" alt=""></img></div>
-                        <div className="flex-item"><b>  Corridors: </b>connections between large patches of highest priority areas and secured lands,
+                    <div className="flex-container2">
+                        <div className="flex-item2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAGdJREFUOI3t1CEOwDAMA8CAScHh/UUfuDzUID8oNhtatNK0aKpRFHAy8iWbc72HqvoKRNITVFXvvd9mVsIiQgAISc+GZiattXJDAJINd+aABzzgz8CIKCNjjBkk6QBygiqZBvb7WM0DvcUmwcYVKdQAAAAASUVORK5CYII=" alt=""></img></div>
+                        <div className="flex-item2"><b>  Corridors: </b>connections between large patches of highest priority areas and secured lands,
                         optimized for efficiency and indicator condition in a least cost path analysis. This
                         category covers an additional 5% of the South Atlantic geography; in total, the Blueprint
                         covers 50%.</div>
@@ -59,13 +59,13 @@ function HomeTab() {
                 </div>
                 <h4>Next Steps</h4>
                 <h4>Basemap Credit</h4>
-                <div className="flex-container">
-                    <div className="flex-item"> <img src={'/images/basemap.png'} height={40} alt=""/></div>
-                    <div className="flex-item"> toggle button and then text attribution</div>
+                <div className="flex-container2">
+                    <div className="flex-item2"> <img src={'/images/basemap.png'} height={40} alt=""/></div>
+                    <div className="flex-item2"> toggle button and then text attribution</div>
                 </div>
-                <div className="flex-container">
-                    <div className="flex-item"> <img src={'/images/basemap2.png'} height={40} alt=""/></div>
-                    <div className="flex-item"> toggle button and then text attribution</div>
+                <div className="flex-container2">
+                    <div className="flex-item2"> <img src={'/images/basemap2.png'} height={40} alt=""/></div>
+                    <div className="flex-item2"> toggle button and then text attribution</div>
                 </div>
                 <h4>Developed By</h4>
                 <p>Conservation Biology Institute</p>
