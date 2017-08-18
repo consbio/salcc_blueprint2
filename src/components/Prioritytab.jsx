1c8655e4df281ebc3acce06cd3794eb293e52322
Natasha Anisimova <anisimon@oregonstate.edu>
2017-08-18 14:19:02 -0700

    Found a bug with priority tab, fixed

--- src/components/Prioritytab.jsx
+++ src/components/Prioritytab.jsx
@@ -25,53 +25,56 @@ function PriorityTab({data}) {
     return (
         <div id = "Content">
             {/*<h2>{data.name}</h2>*/}
+            <div>
             <svg width="100%" height="200">
                 {
                     data.blueprint.map((num, i)=>
                         <rect key={i} x="0" y={position[i]} width="520" height={num*2} fill = {colors[i]}/>
                     )}
             </svg>
-
-                <div className="flex-container2">
-                    <div className="flex-item2">
-                        <svg width ="17" height="17">
-                            <rect width = "100%" height= "100%" fill = "#D3D3D3" stroke="gray" strokeWidth="2"/>
-                        </svg>
-                    </div>
-                    <div className="flex-item2">Not a priority {sum}%</div>
+            </div>
+            <div>
+            <div className="flex-container2">
+                <div className="flex-item2">
+                    <svg width ="17" height="17">
+                        <rect width = "100%" height= "100%" fill = "#D3D3D3" stroke="gray" strokeWidth="2"/>
+                    </svg>
                 </div>
-                <div className="flex-container2">
-                    <div className="flex-item2">
-                        <svg width ="17" height="17">
-                            <rect width = "100%" height= "100%" fill = {colors[1]} stroke="gray" strokeWidth="2"/>
-                        </svg>
-                    </div>
-                    <div className="flex-item2">Corridors {data.blueprint[1]}%</div>
+                <div className="flex-item2">Not a priority {sum}%</div>
+            </div>
+            <div className="flex-container2">
+                <div className="flex-item2">
+                    <svg width ="17" height="17">
+                        <rect width = "100%" height= "100%" fill = {colors[1]} stroke="gray" strokeWidth="2"/>
+                    </svg>
                 </div>
-                <div className="flex-container2">
-                    <div className="flex-item2">
-                        <svg width ="17" height="17">
-                            <rect width = "100%" height= "100%" fill = {colors[2]} stroke="gray" strokeWidth="2"/>
-                        </svg>
-                    </div>
-                    <div className="flex-item2">Medium priority {data.blueprint[2]}%</div>
+                <div className="flex-item2">Corridors {data.blueprint[1]}%</div>
+            </div>
+            <div className="flex-container2">
+                <div className="flex-item2">
+                    <svg width ="17" height="17">
+                        <rect width = "100%" height= "100%" fill = {colors[2]} stroke="gray" strokeWidth="2"/>
+                    </svg>
                 </div>
-                <div className="flex-container2">
-                    <div className="flex-item2">
-                        <svg width ="17" height="17">
-                            <rect width = "100%" height= "100%" fill = {colors[3]} stroke="gray" strokeWidth="2"/>
-                        </svg>
-                    </div>
-                    <div className="flex-item2">High priority {data.blueprint[3]}%</div>
+                <div className="flex-item2">Medium priority {data.blueprint[2]}%</div>
+            </div>
+            <div className="flex-container2">
+                <div className="flex-item2">
+                    <svg width ="17" height="17">
+                        <rect width = "100%" height= "100%" fill = {colors[3]} stroke="gray" strokeWidth="2"/>
+                    </svg>
                 </div>
-                <div className="flex-container2">
-                    <div className="flex-item2">
-                        <svg width ="17" height="17">
-                            <rect width = "100%" height= "100%" fill = {colors[4]} stroke="gray" strokeWidth="2"/>
-                        </svg>
-                    </div>
-                    <div className="flex-item2">Highest priority {data.blueprint[4]}%</div>
+                <div className="flex-item2">High priority {data.blueprint[3]}%</div>
+            </div>
+            <div className="flex-container2">
+                <div className="flex-item2">
+                    <svg width ="17" height="17">
+                        <rect width = "100%" height= "100%" fill = {colors[4]} stroke="gray" strokeWidth="2"/>
+                    </svg>
                 </div>
+                <div className="flex-item2">Highest priority {data.blueprint[4]}%</div>
+            </div>
+            </div>
             <br/>
 
             <h4>Justification</h4>
