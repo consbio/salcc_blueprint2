fd0ada6d28e2aa6607b612a6bd95c1dc07f7cd83
Esti Shay <esti.r.shay@gmail.com>
2018-06-27 17:36:49 -0700

    WIP: add hyperlinks

--- api/report.py
+++ api/report.py
@@ -1,6 +1,7 @@
 import re
 import os
 import json
+import docx
 
 from docx import Document
 # from docx.shared import Inches
@@ -62,9 +63,8 @@ def create_report(id, path):
                 # Where to insert the next partner name
                 p_insert_point = heading
                 for partner in context['partners'][category]:
-                    part = doc.add_paragraph(
-                        partner[0], style='List Bullet'
-                    )
+                    part = doc.add_paragraph(style='List Bullet')
+                    add_hyperlink(part, partner[1], partner[0])
                     # Paragraphs are created at the end of the doc and must be moved into place
                     _move_p_after(part, p_insert_point)
                     p_insert_point = part
@@ -78,15 +78,12 @@ def create_report(id, path):
             _move_p_after(county_header, c_insert_point)
             c_insert_point = county_header
             for county in context['counties']:
-                county_name = doc.add_paragraph(
-                    county['name'], style='List Bullet'
-                )
+                county_name = doc.add_paragraph(style='List Bullet')
+                add_hyperlink(county_name, county['url'], county['name'])
                 _move_p_after(county_name, c_insert_point)
                 c_insert_point = county_name
 
     doc.save(path)
-
-
 
 
 def generate_report_context(id):
@@ -220,6 +217,40 @@ def _move_p_after(p_move, p_destination):
 
     """
     p_destination._p.addnext(p_move._p)
+
+
+def add_hyperlink(paragraph, url, text):
+    """
+    A function that places a hyperlink within a paragraph object.
+
+    :param paragraph: The paragraph we are adding the hyperlink to.
+    :param url: A string containing the required url
+    :param text: The text displayed for the url
+    :return: The hyperlink object
+    """
+
+    # This gets access to the document.xml.rels file and gets a new relation id value
+    part = paragraph.part
+    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
+
+    # Create the w:hyperlink tag and add needed values
+    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
+    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
+
+    # Create a w:r element
+    new_run = docx.oxml.shared.OxmlElement('w:r')
+
+    # Create a new w:rPr element
+    rPr = docx.oxml.shared.OxmlElement('w:rPr')
+
+    # Join all the xml elements together add add the required text to the w:r element
+    new_run.append(rPr)
+    new_run.text = text
+    hyperlink.append(new_run)
+
+    paragraph._p.append(hyperlink)
+
+    return hyperlink
 
 
 def _resolve(scope, key, context):
