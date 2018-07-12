83f84f79e212de6c0feaa8f55841c1cc2a191cbd
Esti Shay <esti.r.shay@gmail.com>
2018-07-12 10:02:45 -0700

    use temporary directory when creating and serving report

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from flask import Flask, render_template, request, send_from_directory, send_file, redirect
 
 from report import create_report
@@ -14,7 +15,7 @@ def main():
 
     if request.method == 'POST':
         data_id = request.form['data_id']
-        return redirect('report/{0}.docx'.format(data_id))
+        return redirect('report/{0}'.format(data_id))
     else:
         return render_template('index.html', data_id='')
 
@@ -23,11 +24,12 @@ def main():
 def get_report(doc_id):
     doc_id = doc_id
     doc_name = 'south_atlantic_blueprint_{version}_{id}.docx'.format(version=BP_VERSION, id=doc_id)
-    path = 'tests/{0}'.format(doc_name)
 
-    create_report(doc_id, path)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = os.path.join(tmpdir, doc_name)
+        create_report(doc_id, path)
 
-    return send_file(path, as_attachment=True)
+        return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
