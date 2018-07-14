1e460739932997fe9c78a913e8da676ca7b330cd
Brendan Ward <bcward@consbio.org>
2018-07-13 17:35:55 -0700

    And now with 100% more pie

--- src/components/PrioritiesTab/index.jsx
+++ src/components/PrioritiesTab/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import PieChart from 'react-minimal-pie-chart'
 
-import LabeledPercentBar from '../Charts/LabeledPercentBar'
+import { formatPercent } from '../../utils'
 import * as actions from '../../Actions/actions'
 import PRIORITIES from '../../config/priorities.json'
 import PLANS from '../../config/plans.json'
@@ -10,12 +11,6 @@ import PLANS from '../../config/plans.json'
 // sort priorities from highest to lowest
 const INLAND_PRIORITIES = [5, 4, 3, 2, 1, 0]
 const MARINE_PRIORITIES = [5, 4, 3, 2, 0]
-
-const renderPriority = (priority, percent) => {
-    const { label, color } = PRIORITIES[priority]
-
-    return <LabeledPercentBar key={priority} label={label} percent={percent} color={color} height={6} />
-}
 
 const renderPlan = (plan) => {
     const { label, url } = PLANS[plan]
@@ -40,13 +35,35 @@ const PrioritiesTab = ({
     const statePlans = plans.filter(p => PLANS[p].type === 'state')
     const marinePlans = plans.filter(p => PLANS[p].type === 'marine')
     const priorities = isMarine ? MARINE_PRIORITIES : INLAND_PRIORITIES
+    const chartData = priorities
+        .map(p => ({
+            value: blueprint[p],
+            label: PRIORITIES[p].label,
+            color: PRIORITIES[p].color
+        }))
+        .filter(p => p.value > 0)
+
+    console.log(blueprint, chartData)
 
     return (
         <div id="Content" className="flex-container-column">
             <section>
                 <h3>Blueprint 2.2 Priority</h3>
                 <h4>for shared conservation action</h4>
-                {priorities.map(p => renderPriority(p, blueprint[p]))}
+                <div className="chart-pie flex-container flex-align-center">
+                    <PieChart data={chartData} style={{ width: 200 }} />
+                    <div className="chart-pie-legend">
+                        {chartData.map(priority => (
+                            <div className="chart-pie-legend-row flex-container flex-align-center">
+                                <div className="chart-pie-legend-patch" style={{ backgroundColor: priority.color }} />
+                                <div className="chart-pie-legend-label text-smaller">
+                                    {priority.label}
+                                    <span className="text-quieter text-small"> ({formatPercent(priority.value)}%)</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </section>
 
             {justification && (
